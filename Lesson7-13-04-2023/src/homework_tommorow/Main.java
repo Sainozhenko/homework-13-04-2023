61f553c467d5d48a2fed297a09e30e884c7766e9
Dmytro Sainozhenko <126772772+Sainozhenko@users.noreply.github.com>
2023-04-13 20:03:01 +0200

    Update Main.java

--- Lesson7-13-04-2023/src/homework_tommorow/Main.java
+++ Lesson7-13-04-2023/src/homework_tommorow/Main.java
@@ -8,14 +8,24 @@ public class Main {
         String abc1 ="abca";
         StringConcate sc = (a, b) -> String.valueOf(a) + (b);
         System.out.println(sc.concat(5, 5));
-
+       
+        Output:
+        55
+            
         //2
         Checkable check11 = in -> in.length() == 3;
         System.out.println(check11.check(abc));
         System.out.println(check11.check(abc1));
-
+        
+        Output:
+        true
+        false
+              
         //3
         Printable pr = in -> System.out.println("!" + in + "!");
         pr.print(abc);
+       
+        Output:        
+        !abc!
         }
     }
