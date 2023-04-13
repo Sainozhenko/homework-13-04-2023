af6fa05c971bb061a373b1595d5e224a1f09d7a3
Dmytro Sainozhenko <126772772+Sainozhenko@users.noreply.github.com>
2023-04-13 20:01:26 +0200

    Update Main.java

--- Lesson7-13-04-2023/src/homework_tommorow/Main.java
+++ Lesson7-13-04-2023/src/homework_tommorow/Main.java
@@ -3,19 +3,19 @@ package homework_tommorow;
 
 public class Main {
     public static void main(String[] args) {
-String abc ="abc";
-String abc1 ="abca";
+        //1
+        String abc ="abc";
+        String abc1 ="abca";
         StringConcate sc = (a, b) -> String.valueOf(a) + (b);
         System.out.println(sc.concat(5, 5));
 
-
+        //2
         Checkable check11 = in -> in.length() == 3;
         System.out.println(check11.check(abc));
         System.out.println(check11.check(abc1));
 
-
+        //3
         Printable pr = in -> System.out.println("!" + in + "!");
         pr.print(abc);
-    }
-
+        }
     }
